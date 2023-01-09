a2ed96999d4c83d59a05a8a397f11bebad1e5bcc
snowflake-learning <122182241+snowflake-learning@users.noreply.github.com>
2023-01-08 22:29:09 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,6 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 import requests
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
