51a9deb6b1a6d952bbd95088b97a6f16c118035b
snowflake-learning <122182241+snowflake-learning@users.noreply.github.com>
2023-01-08 22:37:38 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,12 @@ streamlit.dataframe(fruits_to_show)
 
 
 #New section to display fruityvice api response
-import requests
+
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit_choice")
 #streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 
 
